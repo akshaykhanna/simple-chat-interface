8e9083fb54360f8b383a1528f2544e6b7c02486e
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-06-20 12:05:47 +0530

    Update component file extension from js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import ListChats from './ListChats/ListChats';
 import AddChat from './AddChat/AddChat';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
+    <Provider store={store}>
+
     <div className="App">
       <ListChats />
       <AddChat />
     </div>
+    </Provider>
   );
 }
 
